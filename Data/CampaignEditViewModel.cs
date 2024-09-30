f3b87a7cd0573b759933ee7e738a66ab51ce256d
Mitrozbharat <mitrozbharath@gmail.com>
2024-09-30 11:00:14 +0530

    add new quatation chnages

---
+++ Data/CampaignEditViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hoarding_managment.Data
+{
+    public class CampaigneditViewModel
+    {
+        public int Id { get; set; }
+        public string? BookingAmt { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+    }
+}
